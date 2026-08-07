6d2f3e3b8fe1aa8b5b43efe65f7a5a3eff92cfd0
vasuphol puangjuntuk <vasuphol.p@securitypitch.com>
2026-08-07 09:32:56 +0700

    chore: Remove commented-out MongoDB connection code from db.ts

--- server/db.ts
+++ server/db.ts
@@ -1,20 +1,6 @@
 // server/db.ts
 import mongoose from 'mongoose'
 
-// export const connectDB = async () => {
-//  try {
-//   // เปลี่ยน URL ให้เป็นของ MongoDB ของคุณ (เช่น MongoDB Atlas หรือ Local)
-//   const MONGODB_URI =
-//    'mongodb+srv://baigho123_db_user:kDX4fAhDeyMBGyLX@icu-next-db.rfevjo6.mongodb.net/sample_mflix?retryWrites=true&w=majority'
-
-//   await mongoose.connect(MONGODB_URI)
-//   console.log('✅ MongoDB Connected Successfully')
-//  } catch (error) {
-//   console.error('❌ MongoDB Connection Error:', error)
-//   process.exit(1) // ปิดแอปถ้าต่อ DB ไม่สำเร็จ
-//  }
-
-// }
 let isConnected = false
 export const connectDB = async () => {
  if (isConnected) {
